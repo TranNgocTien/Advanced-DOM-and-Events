9f397ba8e65693d1653da9102129f3c7291dcf6f
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-05-23 22:53:12 +0700

    Event Propagation: Bubbling and Capturing

--- script.js
+++ script.js
@@ -111,40 +111,66 @@ document.addEventListener('keydown', function (e) {
 // logo.classList.contains();
 
 
-const btnScrollTo=document.querySelector('.btn--scroll-to');
-const section1=document.querySelector('#section--1');
+// const btnScrollTo=document.querySelector('.btn--scroll-to');
+// const section1=document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click',function(e){
-  const s1coords=section1.getBoundingClientRect();
-  console.log(s1coords);
-  console.log(e.target.getBoundingClientRect());
+// btnScrollTo.addEventListener('click',function(e){
+//   const s1coords=section1.getBoundingClientRect();
+//   console.log(s1coords);
+//   console.log(e.target.getBoundingClientRect());
 
-  console.log('Current scroll (X/Y)' , window.pageXOffset,pageYOffset);
-  console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+//   console.log('Current scroll (X/Y)' , window.pageXOffset,pageYOffset);
+//   console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
   
-  // Scrolling
-  // window.scrollTo(s1coords.left+window.pageXOffset,s1coords.top+pageYOffset);
+//   // Scrolling
+//   // window.scrollTo(s1coords.left+window.pageXOffset,s1coords.top+pageYOffset);
 
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // })
+//   // window.scrollTo({
+//   //   left: s1coords.left + window.pageXOffset,
+//   //   top: s1coords.top + window.pageYOffset,
+//   //   behavior: 'smooth',
+//   // })
 
-  section1.scrollIntoView({behavior:'smooth'});
+//   section1.scrollIntoView({behavior:'smooth'});
+// });
+
+
+// const h1 =document.querySelector('h1');
+
+// const alertH1=function(e){
+//   alert('addEventListener: Great! You are reading the heading :D'); 
+// }
+
+// h1.addEventListener('mouseenter',alertH1);
+
+// h1.onmouseenter = function(e){
+//   alert('onmouseenter: Great! You are reading the heading :D'); 
+// };
+
+// setTimeout(()=>h1.removeEventListener('mouseenter',alertH1),3000);
+
+
+
+
+//rgb(255,255,255);
+const randomInt = (min,max)=> Math.floor(Math.random()*(max-min+1)+min);
+
+const randomColor= ()=>`rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+document.querySelector('.nav__link').addEventListener('click',function(e){
+    this.style.backgroundColor=randomInt();
+    console.log('LINK',e.target,e.currentTarget);
+    console.log(e.currentTarget===this);
+
+    //Stop propagation
+    e.stopPropagation();
 });
 
+document.querySelector('.nav__links').addEventListener('click',function(e){
+  this.style.backgroundColor=randomInt();
+  console.log('CONTAINER',e.target,e.currentTarget);
+});
 
-const h1 =document.querySelector('h1');
-
-const alertH1=function(e){
-  alert('addEventListener: Great! You are reading the heading :D'); 
-}
-
-h1.addEventListener('mouseenter',alertH1);
-
-h1.onmouseenter = function(e){
-  alert('onmouseenter: Great! You are reading the heading :D'); 
-};
-
-setTimeout(()=>h1.removeEventListener('mouseenter',alertH1),3000);
+document.querySelector('.nav').addEventListener('click',function(e){
+  this.style.backgroundColor=randomInt();
+  console.log('NAV',e.target,e.currentTarget);
+},);
