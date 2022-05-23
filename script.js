11e430d517508eafb5f038c7b81f47715c2392c4
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-05-23 21:00:41 +0700

    implementing Smooth Scrolling

--- script.js
+++ script.js
@@ -36,76 +36,100 @@ document.addEventListener('keydown', function (e) {
 
 
 
-//Lecture
+// //Lecture
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-const header= document.querySelector('.header');
-const allSections=document.querySelectorAll('.section');
-console.log(allSections);
-document.getElementById('section--1');
-const allButtons=document.getElementsByTagName('button');
-console.log(allButtons);
+// const header= document.querySelector('.header');
+// const allSections=document.querySelectorAll('.section');
+// console.log(allSections);
+// document.getElementById('section--1');
+// const allButtons=document.getElementsByTagName('button');
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
-//Creating and inserting elements
-//.insertAdjacentHTML
+// //Creating and inserting elements
+// //.insertAdjacentHTML
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-// message.textContent = `We use cookied for improved functionality and analytics.`;
-message.innerHTML=`We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
-// header.before(message);
-header.after(message);
-//Delete elements
-document.querySelector('.btn--close-cookie').addEventListener('click',function(){
-  message.remove();
-  message.parentElement.removeChild(message);
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent = `We use cookied for improved functionality and analytics.`;
+// message.innerHTML=`We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
+// // header.prepend(message);
+// header.append(message);
+// // header.append(message.cloneNode(true));
+// // header.before(message);
+// header.after(message);
+// //Delete elements
+// document.querySelector('.btn--close-cookie').addEventListener('click',function(){
+//   message.remove();
+//   message.parentElement.removeChild(message);
+// });
+
+
+// //Styles
+// message.style.backgroundColor='#37383d';
+// message.style.width='120%';
+
+// console.log(message.style.color);
+// console.log(message.style.backgroundColor);
+
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+// console.log(getComputedStyle(message));
+
+// message.style.height= Number.parseFloat(getComputedStyle(message).height,10)+30+'px';
+
+// document.documentElement.style.setProperty('--color-primary','orangered');
+
+// //Attributes
+// const logo=document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
+
+// logo.alt='Beautiful minimalist logo';
+// //Non-standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('company','Bankist');
+
+// const link=document.querySelector('.twitter-link');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// //data attributes
+// console.log(logo.dataset.versionNumber);
+
+// //Classes
+// logo.classList.add();
+// logo.classList.remove();
+// logo.classList.toggle();
+// logo.classList.contains();
+
+
+const btnScrollTo=document.querySelector('.btn--scroll-to');
+const section1=document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click',function(e){
+  const s1coords=section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)' , window.pageXOffset,pageYOffset);
+  console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+  
+  // Scrolling
+  // window.scrollTo(s1coords.left+window.pageXOffset,s1coords.top+pageYOffset);
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // })
+
+  section1.scrollIntoView({behavior:'smooth'});
 });
-
-
-//Styles
-message.style.backgroundColor='#37383d';
-message.style.width='120%';
-
-console.log(message.style.color);
-console.log(message.style.backgroundColor);
-
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
-console.log(getComputedStyle(message));
-
-message.style.height= Number.parseFloat(getComputedStyle(message).height,10)+30+'px';
-
-document.documentElement.style.setProperty('--color-primary','orangered');
-
-//Attributes
-const logo=document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
-
-logo.alt='Beautiful minimalist logo';
-//Non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('company','Bankist');
-
-const link=document.querySelector('.twitter-link');
-console.log(link.href);
-console.log(link.getAttribute('href'));
-
-//data attributes
-console.log(logo.dataset.versionNumber);
-
-//Classes
-logo.classList.add();
-logo.classList.remove();
-logo.classList.toggle();
-logo.classList.contains();
