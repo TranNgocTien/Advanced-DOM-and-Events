27db877873bf7cf23b8b9d14532afd06156c3f9a
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-05-24 15:01:47 +0700

    DOM Traversing

--- script.js
+++ script.js
@@ -219,3 +219,29 @@ btnScrollTo.addEventListener('click',function(e){
 //   this.style.backgroundColor=randomInt();
 //   console.log('NAV',e.target,e.currentTarget);
 // },);
+
+const h1=document.querySelector('h1');
+//Going downwards: child
+
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color='white';
+h1.lastElementChild.style.color='orangered';
+
+//Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest('.header').style.background='var(--gradient-secondary)';
+h1.closest('h1').style.background='var(--gradient-primary)';
+
+
+//Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+//move up to parent element, then choose all child
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function(el){
+  if(el!==h1) el.style.transform='scale(0.5)';
+});
