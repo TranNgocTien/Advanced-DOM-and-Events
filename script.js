67a6e0897e11bff51b5d355f438b773784c39601
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-05-25 04:45:38 +0700

    Revealing Elements on Scroll

--- script.js
+++ script.js
@@ -262,10 +262,30 @@ btnScrollTo.addEventListener('click',function(e){
   });
   headerObserver.observe(header);
 
+  // Reveal section
+  const allSections = document.querySelectorAll('.section');
 
+  const revealSection = function(entries,observer){
+    const [entry]=entries;
+    console.log(entry);
 
+    if(!entry.isIntersecting) return;
 
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  }
 
+  const sectionObserver = new IntersectionObserver(revealSection, {
+    root:null,
+    threshold:0.15
+  })
+
+  allSections.forEach(function(section){
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+  });
+
+  
 
 // const h1 =document.querySelector('h1');
 
