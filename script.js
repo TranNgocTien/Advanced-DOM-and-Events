71973a40e9fcc0192b86b37198b1ca9dbd9d9d76
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-05-25 19:59:51 +0700

    Building a Slider Component: Part 2

--- script.js
+++ script.js
@@ -312,8 +312,42 @@ btnScrollTo.addEventListener('click',function(e){
   });
   imgTargets.forEach(img=>imgObserver.observe(img));
 
+  //Slider
 
+  const slides= document.querySelectorAll('.slide');
+  const btnLeft=document.querySelector('.slider__btn--left');
+  const btnRight=document.querySelector('.slide__btn--right');
 
+  let curSlide=0;
+  const maxSlide=slides.length;
+  
+
+  const goToSlide=function(slide){
+    slides.forEach((s,i) => s.style.transform=`translateX(${100 *(i-slide)})`);
+  }
+
+  goToSlide(0);
+
+  const nextSlide=function(){
+    if(curSlide===maxSlide-1){
+      curSlide=0;
+    }else{
+      curSlide++;
+    }
+
+    goToSlide();
+  }
+
+  const prevSlide = function(){
+    if(curSlide===0){
+      curSlide=maxSlide-1;
+    }else{
+      curSlide--;
+    }
+  }
+
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click',prevSlide);
 
 // const h1 =document.querySelector('h1');
 
